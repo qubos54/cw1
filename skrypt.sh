4646e20620269721974fbbe8971f0d469abb26b8
qubos54 <qubos54@gmail.com>
2023-04-25 19:39:41 +0200

    help

--- skrypt.sh
+++ skrypt.sh
@@ -15,6 +15,11 @@ elif [[ "$1" == "--logs" ]]; then
         echo "Nazwa pliku: log$i.txt" >> log$i.txt; echo "Nazwa skryptu: skrypt.sh" >> log$i.txt; echo "Data: $(date)" >> log$i.txt; 
         done
     fi
+elif [[ "$1" == "--help" ]]; then
+    echo "Flagi: --data | --logs | --logs [x]"
+    echo "--date: Wyswietlanie dzisiejszej daty"
+    echo "--logs: Utworzenie 100 plikow logx.txt, gdzie x to numer pliku. W kazdym pliku znajduje sie informacja o: nazwie pliku, nazwe skryptu ktory go utworzyl oraz date utworzenia"
+    echo "--logs [x]: Analogicznie jak wyzej, gdzie x to liczba utworzonych plikow"
 else
     echo "Blad!!"
 fi
