d7ed198f9a8cbe125238f7baa1e7c5117a9f47bc
qubos54 <qubos54@gmail.com>
2023-04-25 19:38:36 +0200

    logs2

--- skrypt.sh
+++ skrypt.sh
@@ -3,11 +3,18 @@
 if [[ "$1" == "--date" ]]; then
     echo "$(date +%d-%m-%Y)"
 elif [[ "$1" == "--logs" ]]; then
-    for i in {1..100}; do 
-        echo "Nazwa pliku: log$i.txt" >> log$i.txt
-        echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
-        echo "Data: $(date)" >> log$i.txt
-    done
+    if [[ "$2" =~ ^[0-9]+$ ]]; then
+        liczba_plikow=$2
+        for (( i=1; i<=$liczba_plikow; i++ )); do
+            echo "Nazwa pliku: log$i.txt" > log$i.txt
+            echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
+            echo "Data: $(date +%d-%m-%Y)" >> log$i.txt
+        done
+    else
+        for i in {1..100}; do 
+        echo "Nazwa pliku: log$i.txt" >> log$i.txt; echo "Nazwa skryptu: skrypt.sh" >> log$i.txt; echo "Data: $(date)" >> log$i.txt; 
+        done
+    fi
 else
     echo "Blad!!"
 fi
